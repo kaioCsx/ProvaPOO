949666a877b3aec28723da42dfcc56d4226bbab8
Kaio <lamonthgames73@gmail.com>
2024-09-11 14:48:22 -0300

    Create Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,19 @@
+package prova.ifs;
+
+public class Menu {
+	
+	//Gerar o menu.
+	public String getMenu() {
+		return "====================== \n"
+				+"     Menu do jogo \n"
+				+"====================== \n"
+				+"Escolha uma opcao: \n"
+				+"1- Cadastrar Espadachim \n"
+				+"2- Cadastrar Arqueiro \n"
+				+"3- Cadastrar Ninja \n"
+				+"4- Cadastrar Gladiador \n"
+				+"5- Cadastar Lutador \n"
+				+"0- Sair";
+	}
+
+}
